1650459da4912f3ee68c92e6b169f72f27501655
Kevin Lai <unurnf@gmail.com>
2014-02-14 11:58:29 -0800

    Refactored a bit of the AI class.

--- Main/src/org/discontinuous/discgame/AI.java
+++ Main/src/org/discontinuous/discgame/AI.java
@@ -5,22 +5,25 @@ import java.util.HashMap;
 import java.util.Map;
 
 /**
- * Created by Urk on 1/28/14.
+ * Opponent AI for the Discontinuity Dialog Conflict Resolution game.
+ * http://www.discontinuous.org/blog/?p=28
+ * Creates a partial game tree of moves for JUST the opponent at the moment.
+ * Refreshing myself on Java, feel like I'm trying to make it work like Ruby.
+ * By Kevin Lai
  */
 
-// TODO: Calculations are done independently of player action, factor in player action at some point.
 public class AI {
     Contestant opponent;
     Contestant player;
     // This is how many steps ahead the AI looks for pathfinding.
     // Exponentially takes more time the more iterations there are, so keep this low
-    // int total is also instantiated each recursion, so space usage will also start adding up.
-    final int iterations = 8; // Above around 9 and you start seeing a noticeable pause... super inefficient
+    final int iterations = 8;
     Map<Cell, Float> possible_moves;
     Cell max_cell;
     float max_value;
 
     // Current AI weights - may change based on personalities, abilities, etc.
+    // TODO: Move out and read from game variables yml
     int dp_weight = 1;
     int backtrack_dp_weight = 3;
     int conf_plus_weight = 2;
@@ -37,7 +40,7 @@ public class AI {
 
     public Cell find_next_move() {
         // Reset max_value, reset adjacent hashmap
-        // Resetting max_cell to null since we want exception rather than hiding with random legal move
+        // Resetting max_cell to null since we want exception rather than hiding errors with random legal move
         max_value = 0;
         max_cell = null;
         possible_moves.clear();
@@ -46,7 +49,7 @@ public class AI {
         for (Cell cell : opponent.adjacent) {
             ArrayList<Cell> previousCells = new ArrayList<Cell>();
             previousCells.add(opponent.cell);
-            // if combo, add in bonus - regardless of cell consumption state
+            // If combo, add in bonus - regardless of cell consumption state
             if (opponent.combo.checkCombo(opponent.cell, cell)) {
                 possible_moves.put(cell, sum_move_options(cell, previousCells) + (getBonuses(opponent.cell)));
             }
@@ -55,11 +58,11 @@ public class AI {
             }
         }
 
-        // Fuckit, naive implementation of getting largest value.  Should really be sorting.
-        // It's O(n) but on four values, nobuddygiffafuk
+        // Naive implementation of getting largest value, it's O(n) on four values, fine for prototype
+        // TODO: Should be sorting.
         for (Map.Entry<Cell, Float> entry: possible_moves.entrySet()) {
-            // Max_cell null ensures that if values go really negative, as in
-            // when AI is surrounded by backtracking, will still pick best route
+            // Max_cell null ensures that if all values go negative, as in
+            // when AI is surrounded by bad squares, will still pick best route
             if (entry.getValue() > max_value || null == max_cell) {
                 max_cell = entry.getKey();
                 max_value = entry.getValue();
@@ -74,8 +77,8 @@ public class AI {
 
         // System for weighing the potential - if all the potential in a route is backloaded, as in,
         // all the good squares are x down the line, value them less than those available immediately
-        // - This can probably be encoded into AI personalities
-        // Current default coefficient is 1 * 0.8^x where x is number of steps
+        // TODO: This can probably be encoded into AI personalities
+        // Current default valuation coefficient is 1 * 0.8^x where x is number of steps
 
         float weight;
         weight = weight(previousCells.size());
@@ -123,37 +126,50 @@ public class AI {
     }
 
     public float getBonuses(Cell cell) {
+        int power = 0;
+        int conf_plus = 0;
+        int conf_minus = 0;
+        int ins_plus = 0;
+        int ins_minus = 0;
+
         // Weight conf_plus more if confidence is low, and like so for each of the stats
         switch (cell.type) {
             case Logical:
-                return (opponent.log_stats.get("power") * dp_weight +
-                // More confidence means getting confidence is less important
-                opponent.log_stats.get("conf_plus") * conf_plus_weight * (1 - ((float) opponent.confidence/opponent.conf_max)) +
-                // Opponent having more confidence means confidence becomes a higher priority
-                opponent.log_stats.get("conf_minus") * conf_minus_weight * ((float) player.confidence/player.conf_max) +
-                // Likewise for inspiration
-                opponent.log_stats.get("ins_plus") * ins_plus_weight * (1 - ((float) opponent.inspiration/opponent.insp_max)) +
-                opponent.log_stats.get("ins_minus") * ins_minus_weight * ((float) player.inspiration/player.insp_max));
+                power = opponent.log_stats.get("power");
+                conf_plus = opponent.log_stats.get("conf_plus");
+                conf_minus = opponent.log_stats.get("conf_minus");
+                ins_plus = opponent.log_stats.get("ins_plus");
+                ins_minus = opponent.log_stats.get("ins_minus");
+                break;
             case Ethical:
-                return (opponent.eth_stats.get("power") * dp_weight +
-                opponent.eth_stats.get("conf_plus") * conf_plus_weight * (1 - ((float) opponent.confidence/opponent.conf_max)) +
-                opponent.eth_stats.get("conf_minus") * conf_minus_weight * ((float) player.confidence/player.conf_max) +
-                opponent.eth_stats.get("ins_plus") * ins_plus_weight * (1 - ((float) opponent.inspiration/opponent.insp_max)) +
-                opponent.eth_stats.get("ins_minus") * ins_minus_weight * ((float) player.inspiration/player.insp_max));
+                power = opponent.eth_stats.get("power");
+                conf_plus = opponent.eth_stats.get("conf_plus");
+                conf_minus = opponent.eth_stats.get("conf_minus");
+                ins_plus = opponent.eth_stats.get("ins_plus");
+                ins_minus = opponent.eth_stats.get("ins_minus");
+                break;
             case Interrogate:
-                return (opponent.ing_stats.get("power") * dp_weight +
-                opponent.ing_stats.get("conf_plus") * conf_plus_weight * (1 - ((float) opponent.confidence/opponent.conf_max)) +
-                opponent.ing_stats.get("conf_minus") * conf_minus_weight * ((float) player.confidence/player.conf_max) +
-                opponent.ing_stats.get("ins_plus") * ins_plus_weight * (1 - ((float) opponent.inspiration/opponent.insp_max)) +
-                opponent.ing_stats.get("ins_minus") * ins_minus_weight * ((float) player.inspiration/player.insp_max));
+                power = opponent.ing_stats.get("power");
+                conf_plus = opponent.ing_stats.get("conf_plus");
+                conf_minus = opponent.ing_stats.get("conf_minus");
+                ins_plus = opponent.ing_stats.get("ins_plus");
+                ins_minus = opponent.ing_stats.get("ins_minus");
+                break;
             case Intimidate:
-                return (opponent.inm_stats.get("power") * dp_weight +
-                opponent.inm_stats.get("conf_plus") * conf_plus_weight * (1 - ((float) opponent.confidence/opponent.conf_max)) +
-                opponent.inm_stats.get("conf_minus") * conf_minus_weight * ((float) player.confidence/player.conf_max) +
-                opponent.inm_stats.get("ins_plus") * ins_plus_weight * (1 - ((float) opponent.inspiration/opponent.insp_max)) +
-                opponent.inm_stats.get("ins_minus") * ins_minus_weight * ((float) player.inspiration/player.insp_max));
+                power = opponent.inm_stats.get("power");
+                conf_plus = opponent.inm_stats.get("conf_plus");
+                conf_minus = opponent.inm_stats.get("conf_minus");
+                ins_plus = opponent.inm_stats.get("ins_plus");
+                ins_minus = opponent.inm_stats.get("ins_minus");
+                break;
         }
-        // TODO: Maybe return an exception or something, should never reach return 0
-        return 0;
+        return (power * dp_weight +
+                // Weighting: More confidence means getting confidence is less important
+                conf_plus * conf_plus_weight * (1 - ((float) opponent.confidence/opponent.conf_max)) +
+                // Opponent having more confidence means confidence becomes a higher priority
+                conf_minus * conf_minus_weight * ((float) player.confidence/player.conf_max) +
+                // Likewise for inspiration
+                ins_plus * ins_plus_weight * (1 - ((float) opponent.inspiration/opponent.insp_max)) +
+                ins_minus * ins_minus_weight * ((float) player.inspiration/player.insp_max));
     }
 }
